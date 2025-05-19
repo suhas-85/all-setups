a07a8b24f1b5a3eef460d0e77241b3b1de39d148
Suhas C <91247659+suhas-85@users.noreply.github.com>
2025-05-19 20:44:21 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -4,7 +4,8 @@ sudo yum update -y
 sudo yum install wget -y
 yum install java-17-amazon-corretto -y
 sudo mkdir /app && cd /app
-sudo wget -O nexus.tar.gz https://download.sonatype.com/nexus/3/latest-unix.tar.gz
+#sudo wget -O nexus.tar.gz https://download.sonatype.com/nexus/3/latest-unix.tar.gznexus-3.80.0-06-linux-x86_64.tar.gz
+sudo wget -O nexus.tar.gz https://download.sonatype.com/nexus/3/nexus-3.80.0-06-linux-x86_64.tar.gz
 sudo tar -xvf nexus.tar.gz
 sudo mv nexus-3* nexus
 sudo adduser nexus
